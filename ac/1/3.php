7c03c6bdc37ba9223062ab79e95c820cc7c779b8
Sistema Audio Cristão <manoel@arquivocristao.com.br>
2017-07-01 22:45:33 -0300

    Create 3.php

---
+++ ac/1/3.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+      <html>
+        <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <title>My Guestbook</title>
+        </head>
+        <body>
+        <h1>Welcome To My Site</h1>
+        <?php include('/home/profetamanoel/public_html/Cadastro/gbinclude.php'); ?>        
+        </body>
+      </html>
